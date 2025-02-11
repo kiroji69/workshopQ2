7a5a9ff589547fb3322e16f7646e1e26059078a3
TaranBourcier <bourciertaran@gmail.com>
2025-02-11 19:06:43 +0100

    ça fonctionne ! LIBERTEEEEEE

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -1,6 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System;
+using Unity.VisualScripting;
+using System.Globalization;
+using System.Drawing;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class Dog : MonoBehaviour
 {
@@ -17,8 +24,14 @@ public class Dog : MonoBehaviour
     private GameObject smallBark;
     private GameObject bigBark;
 
+    SerialPort data_stream = new SerialPort("COM3",9600);
+   public string receivedstring;
+   public float standardBark = 3.0f;
+   public string[] datas;
+
     void Start()
     {
+        data_stream.Open();
         playerRB = GetComponent<Rigidbody2D>();
         smallBark = GameObject.Find("SmallBark");
         bigBark = GameObject.Find("BigBark");
@@ -28,14 +41,18 @@ public class Dog : MonoBehaviour
 
     void Update()
     {
-
+        standardBark =3.0f;
+        receivedstring=data_stream.ReadLine();
+        datas = receivedstring.Split(',');
+        playerRB.transform.position = new Vector3(-7.80f,float.Parse(datas[1], CultureInfo.InvariantCulture)/5f-4.40f,0);
         float vertical = Input.GetAxis("VerticalKey");
 
 
 
 
 
-        // Vertical MVT
+/*     
+         // Vertical MVT
 
         if ((playerRB.position.y < 3.9f && vertical > 0) || (playerRB.position.y > -3.9f && vertical < 0))
         {
@@ -47,34 +64,27 @@ public class Dog : MonoBehaviour
         }
 
 
-
+*/
 
 
         //bark
 
-        bool bark = Input.GetButton("Fire1");
+        bool bark = false;
 
-
-
-        smallBark.SetActive(bark);
-
-        if (bark == true)
-        {
-            barkCount += Time.deltaTime;
-        }
-        if (bark == true && barkCount > 1.0f)
-        {
-            //Debug.Log((barkCount).ToString("00:00.00"));
-            bigBark.SetActive(bark);
+         if (int.Parse(datas[0],CultureInfo.InvariantCulture)>350){
+           
+            while(standardBark>0){
+               standardBark -= Time.deltaTime;
+               smallBark.SetActive(true);
+            }
+            print(bark+","+datas[0]);
+         }
+        
+        else{
             smallBark.SetActive(false);
         }
-
-        if (bark == false)
-        {
-            barkCount = 0;
-            bigBark.SetActive(bark);
-        }
-
+        
+        
 
 
     }
